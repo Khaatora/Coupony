2f724dfa25820602827ff2130d084bbadfdd9830
Khaatora <ahosari20@gmail.com>
2023-05-07 02:54:11 +0300

    added exceptions

--- lib/core/errors/exceptions/api/exceptions.dart
+++ lib/core/errors/exceptions/api/exceptions.dart
@@ -1,8 +1,26 @@
-import 'package:maslaha/core/errors/exceptions/cache_exception.dart';
+import 'package:maslaha/core/errors/exceptions/server_exception.dart';
 
 import '../../../global/localization.dart';
 
-class GenericAPIException extends CacheException{
+class GenericAPIException extends ServerException{
 
   const GenericAPIException([super.message = EnglishLocalization.genericErrorMessage]);
+}
+
+class NoInternetException extends ServerException{
+
+  const NoInternetException([super.message = EnglishLocalization.noInternetErrorMessage]);
+}
+
+class MissingTokenException extends ServerException{
+
+  const MissingTokenException([super.message = EnglishLocalization.missingTokenErrorMessage]);
+}
+class ExpiredTokenException extends ServerException{
+
+  const ExpiredTokenException([super.message = EnglishLocalization.expiredTokenErrorMessage]);
+}
+class InvalidTokenException extends ServerException{
+
+  const InvalidTokenException([super.message = EnglishLocalization.invalidTokenErrorMessage]);
 }
