e24a20eb63c54e846f9d32fb0f1a29c088f16abc
Khaatora <ahosari20@gmail.com>
2023-05-07 02:55:11 +0300

    added forgotPassword route

--- lib/core/constants/routes.dart
+++ lib/core/constants/routes.dart
@@ -5,6 +5,7 @@ class Routes{
   static const home = "/"; // home
   static const login = "/login"; // home
   static const signup = "/signup"; // home
+  static const forgotPassword = "/forgotPassword"; // home
   static const initInfo = "/initInfo"; // page view screen that contains stores, language and region info
 
 }
