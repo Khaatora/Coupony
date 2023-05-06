a2f050d4fc59433465c2233ef986b742c28b4033
Khaatora <ahosari20@gmail.com>
2023-05-07 02:21:57 +0300

    added api_constants

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,5 +2,25 @@
 class ApiConstants{
 
   static const String apiKey = "";
-  static const String baseUrl = "";
+  static const String baseUrl = "https://ec4d-2a09-bac1-2200-18-00-241-6b.ngrok-free.app";
+  // paths
+  static const String verifyTokenPath = "/verify-token";
+  static const String verifyCodePath = "/verify-code";
+  static const String verifyEmailPath = "/verify-email";
+  static const String addUserPath = "/add-user";
+  static const String loginPath = "/login";
+  static const String initiatePasswordResetPath = "/initiate-password-reset";
+  static const String resetPasswordPath = "/reset-password";
+  static const String guestUserPath = "/gt";
+
+
+  static String verifyTokenUrl() => "$baseUrl$loginPath";
+  static String verifyCodeUrl() => "$baseUrl$verifyCodePath";
+  static String verifyEmailUrl() => "$baseUrl$verifyEmailPath";
+  static String addUserUrl() => "$baseUrl$addUserPath";
+  static String loginUrl() => "$baseUrl$loginPath";
+  static String initiatePasswordResetUrl() => "$baseUrl$initiatePasswordResetPath";
+  static String resetPasswordUrl() => "$baseUrl$resetPasswordPath";
+  static String guestUserUrl() => "$baseUrl$guestUserPath";
+
 }
