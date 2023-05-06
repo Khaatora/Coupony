c1b6ca6795571741f1de683b4322cc6edb3f47e8
Khaatora <ahosari20@gmail.com>
2023-05-07 02:16:23 +0300

    addded cache_constants

---
+++ lib/core/constants/cache_constants.dart
@@ -0,0 +1,8 @@
+
+
+class CachedJsonKeys {
+  static const String lang = "lang";
+  static const String region = "region";
+  static const String token = "token";
+  static const String cachedLoggedInUserDataKey = "cachedLoggedInUserData";
+}
