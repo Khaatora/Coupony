cb53de5b2b4675eccb4eebf70103889a98ec99b4
Khaatora <ahosari20@gmail.com>
2023-05-07 02:35:36 +0300

    theme

--- lib/core/global/theme.dart
+++ lib/core/global/theme.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
-class AppTheme{
-
+class AppTheme {
   static final light = ThemeData(
-
-      );
-  static final dark = ThemeData(
-
-      );
+    useMaterial3: true,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll<Color>(
+            HexColor("#00AF87"),
+        ),
+        foregroundColor: const MaterialStatePropertyAll<Color>(
+          Colors.white,
+        ),
+      ),
+    ),
+    textTheme: TextTheme(
+        displaySmall: TextStyle(
+            fontSize: 16,
+            color: HexColor("#00AF87"),
+        ),
+        displayMedium: TextStyle(
+            fontSize: 24,
+            color: HexColor("#00AF87"),
+        )
+    ),
+  );
+  static final dark = ThemeData(useMaterial3: true);
 }
