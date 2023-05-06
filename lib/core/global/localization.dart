4e986d00369d0a2b7dc98866c942507bee04928e
Khaatora <ahosari20@gmail.com>
2023-05-07 02:37:53 +0300

    localization

--- lib/core/global/localization.dart
+++ lib/core/global/localization.dart
@@ -1,3 +1,16 @@
 class EnglishLocalization {
   static const String genericErrorMessage = "something went wrong, please try again";
+  static const String login = "Login";
+  static const String email = "Email";
+  static const String password = "password";
+  static const String forgotPassword = "Forgot Password?";
+  static const String createAnAccount = "Create an Account";
+  static const String enterValidEmail = "Enter valid Email";
+  static const String enterValidPassword = "Enter valid password";
+
+  // exception messages
+  static const String noInternetErrorMessage = "Make sure you are connected to the internet and try again.";
+  static const String missingTokenErrorMessage = "Token is missing!";
+  static const String expiredTokenErrorMessage = "Token is expired!";
+  static const String invalidTokenErrorMessage = "Token is invalid!";
 }
