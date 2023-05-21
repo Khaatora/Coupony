e202a7ed3404fed1d7ee3b9cbb6b7c43b59fd400
ayeldreney <60024298+ayeldreney@users.noreply.github.com>
2023-05-21 08:10:04 +0300

    custom nav bar added

---
+++ lib/core/componets/custom_navigation_bar.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:maslaha/core/global/colors.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  final primaryColor = AppColor.primaryColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient:
+            LinearGradient(colors: [Color(0xff4338CA), Color(0xff6D28D9)]),
+      ),
+      child: BottomAppBar(
+        elevation: 0,
+        color: Colors.transparent,
+        child: SizedBox(
+          height: 56,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconBottomBar(
+                    text: "Home",
+                    icon: Icons.home,
+                    selected: true,
+                    onPressed: () {}),
+                IconBottomBar(
+                    text: "price tag",
+                    icon: Icons.home,
+                    selected: false,
+                    onPressed: () {}),
+                IconBottomBar(
+                    text: "favorite",
+                    icon: Icons.home,
+                    selected: false,
+                    onPressed: () {}),
+                IconBottomBar(
+                    text: "person",
+                    icon: Icons.home,
+                    selected: false,
+                    onPressed: () {}),
+                IconBottomBar(
+                    text: "menu",
+                    icon: Icons.home,
+                    selected: false,
+                    onPressed: () {})
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class IconBottomBar extends StatelessWidget {
+  const IconBottomBar(
+      {Key? key,
+      required this.text,
+      required this.icon,
+      required this.selected,
+      required this.onPressed})
+      : super(key: key);
+  final String text;
+  final IconData icon;
+  final bool selected;
+  final Function() onPressed;
+
+  final primaryColor = const Color(0xff4338CA);
+  final accentColor = const Color(0xffffffff);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          icon:
+              Icon(icon, size: 25, color: selected ? accentColor : Colors.grey),
+        ),
+        Text(
+          text,
+          style: TextStyle(
+              fontSize: 12,
+              height: .1,
+              color: selected ? accentColor : Colors.grey),
+        )
+      ],
+    );
+  }
+}
