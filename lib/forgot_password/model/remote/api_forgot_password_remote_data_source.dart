c665233cd457913466511b1885250bbbecd989ce
Khaatora <ahosari20@gmail.com>
2023-06-10 00:32:01 +0300

    rename

--- lib/forgot_password/model/remote/api_forgot_password_remote_data_source.dart
+++ lib/forgot_password/model/remote/api_forgot_password_remote_data_source.dart
@@ -6,20 +6,24 @@ import 'package:maslaha/forgot_password/model/initiate_password_reset_response.d
 import 'package:maslaha/forgot_password/model/reset_password_response.dart';
 import 'package:maslaha/forgot_password/repository/i_forgot_password_repository.dart';
 
+import '../../../core/MVVM/model/code_verification_response.dart';
 import '../../../core/constants/api_constants.dart';
 import '../../../core/errors/exceptions/api/exceptions.dart';
 import '../../../core/services/services_locator.dart';
 
-abstract class IRemoteDataSource{
+abstract class IForgotPasswordRemoteDataSource{
 
   Future<InitiatePasswordResetResponse> initiatePasswordReset(InitiatePasswordResetParams params);
+
+  //verify-code
+  Future<CodeVerificationResponse> verifyCode(CodeVerificationParams params);
 
   Future<ResetPasswordResponse> resetPassword(ResetPasswordParams params);
 
 }
 
 
-class APIRemoteDataSource extends IRemoteDataSource{
+class APIForgotPasswordRemoteDataSource extends IForgotPasswordRemoteDataSource{
   @override
   Future<InitiatePasswordResetResponse> initiatePasswordReset(InitiatePasswordResetParams params) async {
     final response = await sl<Dio>().post(ApiConstants.initiatePasswordResetPath,
@@ -30,12 +34,30 @@ class APIRemoteDataSource extends IRemoteDataSource{
     switch (response.statusCode) {
       case 200:
         return InitiatePasswordResetResponse.fromJson(response.data);
-    // TODO: other cases
-    // case 307:
+      case 307:
+        throw const InvalidEmailException();
       default:
         throw const GenericAPIException();
     }
 
+  }
+
+  @override
+  Future<CodeVerificationResponse> verifyCode(CodeVerificationParams params) async {
+    final response = await sl<Dio>().post(ApiConstants.verifyCodePath,
+        data: jsonEncode(params.toJson()),
+        options: Options(headers: {
+          HttpHeaders.contentTypeHeader: "application/json",
+        }));
+
+    switch (response.statusCode) {
+      case 200:
+        return CodeVerificationResponse.fromJson(response.data);
+      case 304:
+        throw const IncorrectVerificationCodeException();
+      default:
+        throw const GenericAPIException();
+    }
   }
 
   @override
@@ -48,9 +70,10 @@ class APIRemoteDataSource extends IRemoteDataSource{
     switch (response.statusCode) {
       case 200:
         return ResetPasswordResponse.fromJson(response.data);
-    // TODO: other cases
-    // case 301:
-    // case 308:
+      case 301:
+        throw const SessionNotVerifiedException();
+      case 308:
+        throw const PasswordAlreadyUsedException();
       default:
         throw const GenericAPIException();
     }
